d5bfc6fef5542ee4e35496b284cc0cd307a6ee2b
Dan Huynh <danhuynh99@gmail.com>
2020-04-06 11:23:32 -0500

    Restructure to be MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dan Huynh on 4/5/20.
+//  Copyright © 2020 Dan Huynh. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+
